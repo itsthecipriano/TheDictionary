1a9162571138315abc4e68688a6960f179976bfb
itsthecipriano <itsthecipriano@users.noreply.github.com>
2021-02-17 13:18:53 +0300

    Add project files.

---
+++ TheDictionary/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TheDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<int, string> theSurname = new Dictionary<int, string>();
+            theSurname.Add(25, "Atmaca");
+            theSurname.Add(21, "Arslan");
+            theSurname.Add(19, "Şahin");
+
+
+            Surname<int, string> surname = new Surname<int, string>();
+            surname.Add(17, "Koç");
+
+            surname.List();
+            int result = surname.Count;
+            Console.WriteLine(result);
+
+        }
+    }
+}
---
+++ TheDictionary/Surname.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TheDictionary
+{
+    class Surname<TKey, TValue>
+    {
+        TKey[] Keys;
+        TValue[] Values;
+        public Surname()
+        {
+            Keys = new TKey[0];
+            Values = new TValue[0];
+        }
+
+        public void Add(TKey Key, TValue Value)
+        {
+            TKey[] tArKey = Keys;
+            Keys = new TKey[Keys.Length + 1];
+            TValue[] tArVal = Values;
+            Values = new TValue[Values.Length + 1];
+
+            for (int i = 0; i < tArKey.Length; i++)
+            {
+                Keys[i] = tArKey[i];
+                Values[i] = tArVal[i];
+            }
+
+            Keys[Keys.Length - 1] = Key;
+            Values[Values.Length - 1] = Value;
+        }
+
+        public int Count 
+        {
+            get { return Keys.Length; }
+        }
+
+        public void List() 
+        {
+            for (int i = 0; i < Keys.Length; i++)
+            {
+                Console.WriteLine(Keys[i] + "  " + Values[i]);
+            }
+        }
+    }
+}
